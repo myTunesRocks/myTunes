36bf6c05facf094a1448d3282a2eae69b807b751
Blake Guilloud <fbguillo@gmail.com>
2015-11-05 18:19:40 -0500

    blue

--- client/app.js
+++ client/app.js
@@ -2,30 +2,46 @@ $(document).ready(function(){
   page.init();
 });
 
-var genreData;
-
 var page = {
-
-  genreUrl: '/get-genre',
+  genreUrl: '/get-genres',
   artistUrl: '/get-artist',
   albumUrl: '/get-album',
 
   init: function(){
-    page.loadGenre();
+    page.styling();
+    page.events();
   },
   styling: function(){
-
+    page.loadGenre();
+    page.loadArtist();
+    page.loadAlbum();
   },
   events: function(){
-
+    page.submitGenre();
+    page.submitArtist();
+    page.submitAlbum();
   },
 
   loadGenre: function(){
     $.ajax({
-      url: '/get-genre',
+      url: page.genreUrl,
       method: 'GET',
       success: function(data){
-        console.log('SUCCESS', data);
+        newData = JSON.parse(data);
+        console.log('SUCCESS LOAD GENRE', newData);
+        _.each(newData, function(dog, idx, array){
+
+          $('.blue').append(
+              dog.genreName
+              + ' '
+              + idx
+              + '<img src = ' + dog.image + '>'
+              + ' <br>'
+
+
+          )
+        })
+        // $('.blue').html(newData[0].genreName)
       },
       failure: function(data){
         console.log('FAILURE', data)
@@ -35,10 +51,10 @@ var page = {
 
   loadArtist: function(){
     $.ajax({
-      url: loadContent.artistUrl,
+      url: '/get-artists',
       method: 'GET',
       success: function(data){
-
+        console.log('SUCCESS LOAD ARTIST', JSON.parse(data));
       },
       failure: function(data){
 
@@ -48,9 +64,10 @@ var page = {
 
   loadAlbum(){
     $.ajax({
-      url: loadContent.albumUrl,
+      url: '/get-albums',
       method: 'GET',
       success: function(data){
+        console.log('SUCCESS LOAD ALBUM', JSON.parse(data));
 
       },
       failure: function(data){
@@ -58,5 +75,57 @@ var page = {
       }
   });
   },
+
+    submitGenre: function(){
+      $('body').on('click','.sampleBtn' ,function(event){
+        event.preventDefault();
+          var genreData = {
+            genreName: $('input[name="name"]').val(),
+            genreImage: $('input[name="image"]').val()
+          };
+          $.ajax({
+          url: '/create-genre',
+          method: 'POST',
+          data: genreData,
+          success: function(data){
+            console.log('SUCCESS', data)
+
+          },
+          failure: function(data){
+
+          },
+        });
+      });
+    },
+
+    submitArtist: function(){
+      $('button').on('submit', function(){
+        $.ajax({
+          url: '/create-artist',
+          method: 'POST',
+          success: function(data){
+
+          },
+          failure: function(data){
+
+          },
+        });
+      });
+    },
+
+    submitAlbum: function(){
+      $('button').on('submit', function(){
+        $.ajax({
+          url: '/create-album',
+          method: 'POST',
+          success: function(data){
+
+          },
+          failure: function(data){
+
+          },
+        });
+      });
+    },
 
 };
